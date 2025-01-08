d8f38d1bc01024d71e1c1b330d3211665ed6f0d6
Murtaza <murtazaansari242@gmail.com>
2025-01-08 19:57:51 +0500

    project started

---
+++ tourism/src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./home/home.js";
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="home">
+              <Route index element={<Home />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
