99d7687b840d5b1b855fa673a89f471835ccbe89
Murtaza <murtazaansari242@gmail.com>
2025-05-05 22:56:15 +0500

    project complete

--- tourism frontend/src/App.js
+++ tourism frontend/src/App.js
@@ -9,9 +9,37 @@ import HotelDashboard from "./dashboard/hotel/HotelDashboard.js";
 import AirlineDashboard from "./dashboard/airline/AirlineDashboard.js";
 import GuideDashboard from "./dashboard/guide/GuideDashboard.js";
 import RentalDashboard from "./dashboard/rental/RentalDashboard.js";
-//import { useState, useEffect } from "react";
+// import { useEffect, useState } from "react";
 
 function App() {
+  // const [roleID, setRoleID] = useState("");
+  // useEffect(() => {
+  //   if (
+  //     localStorage.getItem("loggedIn") &&
+  //     localStorage.getItem("loggedIn") === true &&
+  //     localStorage.getItem("email")
+  //   ) {
+  //     fetch("http://localhost:8008/Tourism/UserDataRetreival", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify({ email: localStorage.getItem("email") }),
+  //     })
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         if (data.code === 200) {
+  //           console.log(data.data);
+  //           setRoleID(data.data[0].role_ID);
+  //         } else {
+  //           console.log("Data not retreived!", data.data);
+  //         }
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //       });
+  //   }
+  // }, []);
   return (
     <div className="App">
       <BrowserRouter>
